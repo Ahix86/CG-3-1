d72e6a4c8ce8c3d2f4960d92f4cab37e99fae797
Allison <allison.hix86@gmail.com>
2018-07-24 18:45:22 -0500

    Months

--- CG 3-1/Program.cs
+++ CG 3-1/Program.cs
@@ -6,7 +6,73 @@ namespace CG_3_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Hello, please type the number of a month: ");
+            string userValue = Console.ReadLine();
+            string message = "";
+
+            if (userValue == "1")
+            {
+                message = "January";
+            }
+            else if (userValue == "2")
+            {
+                message = "February";
+            }
+            else if (userValue == "3")
+            {
+                message = "March";
+            }
+            else if (userValue == "4")
+            {
+                message = "April";
+            }
+            else if (userValue == "5")
+            {
+                message = "May";
+            }
+            else if (userValue == "6")
+            {
+                message = "June";
+            }
+            else if (userValue == "7")
+            {
+                message = "July";
+            }
+            else if (userValue == "8")
+            {
+                message = "August";
+            }
+            else if (userValue == "9")
+            {
+                message = "September";
+            }
+            else if (userValue == "10")
+            {
+                message = "October";
+            }
+            else if (userValue == "11")
+            {
+                message = "November";
+            }
+            else if (userValue == "12")
+            {
+                message = "December";
+            }
+                    else
+            { 
+                message = "Please select a number between 1 and 12.";
+            }
+            Console.WriteLine(message);
+            Console.ReadLine();
+
         }
     }
 }
+
+
+
+
+
+     
+
+        
